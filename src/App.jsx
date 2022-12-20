f1550f84cd2baf0139968504905d895d64071983
Inga <piavka91@gmail.com>
2022-12-20 16:55:52 -0300

    add Tariff pproject

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
 
 import './App.scss';
 import Tariff from './components/Tariff';
-
+import { useState } from 'react';
 
 
 const tariffs = [
-  { price: "300", speed: "10", color: "blue" },
-  { price: "450", speed: "50", color: "green" },
-  { price: "550", speed: "100", color: "red" },
-  { price: "1000", speed: "200", color: "black" }
+  { price: "300", speed: "10", color: "blue", id: 1 },
+  { price: "450", speed: "50", color: "green", id: 2 },
+  { price: "550", speed: "100", color: "red", id: 3 },
+  { price: "1000", speed: "200", color: "black", id: 4 }
 ];
 
 function App() {
-
+  const [activeTariffId, setActiveTariffId] = useState(0);
   
     return (
       <div className="App">
         {tariffs.map((tariff) => {
-        return <Tariff price={tariff.price} speed={tariff.speed} color={tariff.color}/>
+          
+        return <Tariff 
+        key={tariff.id}
+        price={tariff.price} 
+        speed={tariff.speed} 
+        color={tariff.color} 
+        id={tariff.id}
+        isActive={activeTariffId === tariff.id} 
+        onCardClick={(id)=> {
+          if (id === activeTariffId) {
+            id = 0;
+          }
+          setActiveTariffId(id);
+        }}/>
         })}
       </div>
   );
--- src/components/Tariff.jsx
+++ src/components/Tariff.jsx
@@ -3,38 +3,28 @@ import './Colors.scss';
 import React from "react";
 
 export default class Tariff extends React.Component {
-    constructor(props) {
-	    super(props);
-	    this.state = {
-	      isActive: false,
-	    };
-	  };
     
-    onClick = () => {
-    this.setState({
-        isActive: !this.state.isActive,
-    });
-  };
+    
 
 render() {
     const {...props} = this.props;
-    const { isActive } = this.state;
     
+
     return (
-    <div className="tariff" onClick={this.onClick}>
-        <div className={`name name_${props.color} ${isActive ? 'name__dedicated':''}`}>
+    <div className="tariff" onClick={() => props.onCardClick(props.id)}>
+        <div className={`name name_${props.color} ${props.isActive ? 'name__dedicated':''}`}>
             <p>Безлимитный {props.price}</p>
         </div>
-        <div className={`price price_${props.color} ${isActive ? 'price__dedicated':''}`}>
+        <div className={`price price_${props.color} ${props.isActive ? 'price__dedicated':''}`}>
             <div>
                 <p className="price_currency">руб</p>
                 <p className="price_month"><span className="price_size">{props.price}</span>/мес</p>
             </div>
         </div>
-        <div className={`speed ${isActive ? 'speed__dedicated':''}`}>
+        <div className={`speed ${props.isActive ? 'speed__dedicated':''}`}>
             <p>до {props.speed} Мбит/сек</p>
         </div>
-        <div className={`info ${isActive ? 'info__dedicated':''}`}>
+        <div className={`info ${props.isActive ? 'info__dedicated':''}`}>
             <p>Объем включенного трафика <br/>
             не ограничен</p>
         </div>
