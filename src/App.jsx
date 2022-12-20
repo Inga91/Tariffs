fa7128e3497e9a02c20ebf9f225eb9629f987d83
Inga <piavka91@gmail.com>
2022-12-20 16:00:05 -0300

    add states

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import './App.scss';
 import Tariff from './components/Tariff';
 
 
+
 const tariffs = [
   { price: "300", speed: "10", color: "blue" },
   { price: "450", speed: "50", color: "green" },
-  { price: "550", speed: "100", color: "red", isActive: true },
+  { price: "550", speed: "100", color: "red" },
   { price: "1000", speed: "200", color: "black" }
 ];
 
 function App() {
+
+  
     return (
       <div className="App">
         {tariffs.map((tariff) => {
-        return <Tariff price={tariff.price} speed={tariff.speed} color={tariff.color} isActive={tariff.isActive}/>
+        return <Tariff price={tariff.price} speed={tariff.speed} color={tariff.color}/>
         })}
       </div>
   );
--- src/components/Tariff.jsx
+++ src/components/Tariff.jsx
@@ -9,24 +9,32 @@ export default class Tariff extends React.Component {
 	      isActive: false,
 	    };
 	  };
+    
+    onClick = () => {
+    this.setState({
+        isActive: !this.state.isActive,
+    });
+  };
+
 render() {
     const {...props} = this.props;
+    const { isActive } = this.state;
     
     return (
-    <div className="tariff">
-        <div className={`name name_${props.color} ${props.isActive ? 'name__dedicated':''}`}>
+    <div className="tariff" onClick={this.onClick}>
+        <div className={`name name_${props.color} ${isActive ? 'name__dedicated':''}`}>
             <p>Безлимитный {props.price}</p>
         </div>
-        <div className={`price price_${props.color} ${props.isActive ? 'price__dedicated':''}`}>
+        <div className={`price price_${props.color} ${isActive ? 'price__dedicated':''}`}>
             <div>
                 <p className="price_currency">руб</p>
                 <p className="price_month"><span className="price_size">{props.price}</span>/мес</p>
             </div>
         </div>
-        <div className={`speed ${props.isActive ? 'speed__dedicated':''}`}>
+        <div className={`speed ${isActive ? 'speed__dedicated':''}`}>
             <p>до {props.speed} Мбит/сек</p>
         </div>
-        <div className={`info ${props.isActive ? 'info__dedicated':''}`}>
+        <div className={`info ${isActive ? 'info__dedicated':''}`}>
             <p>Объем включенного трафика <br/>
             не ограничен</p>
         </div>
